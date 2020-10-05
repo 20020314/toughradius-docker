79289dfce7c9e2d8322e34d508e4a72f00065c1a
jamiesun <jamiesun.net@gmail.com>
2020-10-05 20:48:09 +0800

    2020-10-05 20:48:09

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /opt/toughradius
+test -d /opt/toughradius || cd /opt/toughradius
 
 # 下载容器部署描述文件
 curl https://raw.githubusercontent.com/toughstruct/toughradius-docker/master/docker-compose.yml -o docker-compose.yml
@@ -14,8 +14,15 @@ docker-compose pull
 # 首先创建mysql容器
 docker-compose up -d mysql
 
-# 等待 mysql 初始化完成后再执行sql
-sleep 3
+sleep 2
+# 等待 3秒 mysql 初始化完成后再执行sql
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+
 
 # 创建数据库表
 mysql -h 127.0.0.1 -uroot -pmyroot < ./toughradius.sql
